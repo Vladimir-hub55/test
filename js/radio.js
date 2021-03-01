80a9aaa214e396cbb812569355d25ba6b212c026
Vladimir-hub55 <vladimiraninin@gmail.com>
2021-03-01 20:13:44 +0200

    Картинки с сервера

--- js/radio.js
+++ js/radio.js
@@ -2,10 +2,12 @@ function format() {
     var rad = document.getElementsByName('view');
     var treeView = document.querySelector('.tree-view');
     var cards = document.querySelector('.cards-block');
+    var pag = document.querySelector('.pag');
     for (var i = 0; i < rad.length; i++) {
         if (rad[i].checked) {
             treeView.classList.remove('clear');
             cards.classList.add('clear');
+            $('.pag').toggleClass('paginator');
         } else {
             treeView.classList.add('clear');
             cards.classList.remove('clear');
